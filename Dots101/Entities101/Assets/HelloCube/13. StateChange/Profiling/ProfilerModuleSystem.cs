6fcb0f1dc0ff156c9346b0e4a62f30181f61028f
PhuNope <mrphu2001@gmail.com>
2026-01-22 17:38:33 +0700

    Add StateChange

---
+++ Dots101/Entities101/Assets/HelloCube/13. StateChange/Profiling/ProfilerModuleSystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+
+#if UNITY_EDITOR
+
+namespace HelloCube.StateChange {
+    public partial struct ProfilerModuleSystem : ISystem {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state) {
+            state.RequireForUpdate<ExecuteStateChange>();
+        }
+
+        public void OnUpdate(ref SystemState state) {
+            ref var frameData = ref SystemAPI.GetSingletonRW<StateChangeProfilerModule.FrameData>().ValueRW;
+            StateChangeProfilerModule.SpinPerf = frameData.SpinPerf;
+            StateChangeProfilerModule.UpdatePerf = frameData.SetStatePerf;
+        }
+    }
+}
+
+#endif
