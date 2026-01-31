da03d16e0cecaacfc26acca5a5340bd9a99f48d0
PhuNope <mrphu2001@gmail.com>
2026-02-01 01:14:43 +0700

    UnityObjectRef

---
+++ Dots101/Entities101/Assets/HelloCube/15. UnityObjectRef/AnimatorAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class AnimatorAuthoring : MonoBehaviour {
+    public GameObject AnimatorPrefab;
+
+    class AnimatorBaker : Baker<AnimatorAuthoring> {
+        public override void Bake(AnimatorAuthoring authoring) {
+            var e = GetEntity(TransformUsageFlags.Renderable);
+            AddComponent(e, new AnimatorRefConponent() { AnimatorAsGO = authoring.AnimatorPrefab });
+        }
+    }
+
+    public struct AnimatorRefConponent : IComponentData {
+        public UnityObjectRef<GameObject> AnimatorAsGO;
+    }
+}
---
+++ Dots101/Entities101/Assets/HelloCube/15. UnityObjectRef/SpawnAnimatedCubeSystem.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public partial struct SpawnAnimatedCubeSystem : ISystem {
+    private EntityQuery m_AnimatorRefComponentQuery;
+
+    public void OnCreate(ref SystemState state) {
+        m_AnimatorRefComponentQuery = SystemAPI.QueryBuilder()
+            .WithAll<AnimatorAuthoring.AnimatorRefConponent>()
+            .WithNone<Animator>()
+            .Build();
+
+        state.RequireForUpdate(m_AnimatorRefComponentQuery);
+    }
+
+    public void OnUpdate(ref SystemState state) {
+        var entities = SystemAPI.QueryBuilder()
+            .WithAll<AnimatorAuthoring.AnimatorRefConponent>()
+            .WithNone<Animator>()
+            .Build()
+            .ToEntityArray(state.WorldUpdateAllocator);
+
+        foreach (var entity in entities) {
+            // Get the animator reference
+            var animRef = SystemAPI.GetComponent<AnimatorAuthoring.AnimatorRefConponent>(entity);
+
+            // Instantiate the GO
+            var rotatingCube = (GameObject)Object.Instantiate(animRef.AnimatorAsGO);
+
+            // Add the animator to the entity
+            state.EntityManager.AddComponentObject(entity, rotatingCube.GetComponent<Animator>());
+        }
+    }
+}
