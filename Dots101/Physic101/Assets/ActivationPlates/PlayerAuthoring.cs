a39f96235b4e6a7e5efa1adf1ef14f0fa071c07e
PhuNope <mrphu2001@gmail.com>
2026-05-27 16:48:28 +0700

    Add ActivationPlates

---
+++ Dots101/Physic101/Assets/ActivationPlates/PlayerAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace ActivationPlates {
+    public class PlayerAuthoring : MonoBehaviour {
+        private class PLayerAuthoringBaker : Baker<PlayerAuthoring> {
+            public override void Bake(PlayerAuthoring authoring) {
+                var entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
+
+                AddComponent<Player>(entity);
+            }
+        }
+    }
+
+    public struct Player : IComponentData {
+    }
+}
---
+++ Dots101/Physic101/Assets/ActivationPlates/PlayerSystem.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace ActivationPlates {
+    public partial struct PlayerSystem : ISystem {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state) {
+            state.RequireForUpdate<Config>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state) {
+            var config = SystemAPI.GetSingleton<Config>();
+            float3 input = new float3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            var speed = config.PlayerMoveSpeed * SystemAPI.Time.DeltaTime;
+
+            foreach (var playerTransform in SystemAPI.Query<RefRW<LocalTransform>>().WithAll<Player>()) {
+                playerTransform.ValueRW.Position += input * speed;
+            }
+        }
+    }
+}
